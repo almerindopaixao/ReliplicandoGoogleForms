1500b14ab08ae211a596741618adc52a6d8864c5
Almerindo Paixão <almerindopaixao@gmail.com>
2020-08-11 19:52:06 -0300

    update layout

--- index.php
+++ index.php
@@ -10,22 +10,26 @@
 </head>
 <body>
   <main id="root">
+    <div class="background"></div>
     <form 
       action="src/controllers/cadCliente.php" 
       method="POST"
       name="registrationForm"
       class="registrationForm"
     >
-      <h1>Cadastro de clientes</h1>
+      <div class="container-block title">
+        <h1>Cadastro de Clientes</h1>
+        <p>Preencha este formulário para se cadastrar em nossa plataforma</p>
+      </div>
 
-      <div class="input-block">
+      <div class="container-block">
         <label for="name">
           Nome
           <input id="name" type="text" name="name">
         </label>
       </div>
 
-      <div class="input-block">
+      <div class="container-block">
         <label for="cpf">
           CPF
           <input 
@@ -37,113 +41,110 @@
         </label>
       </div>
 
-      <div class="input-block">
+      <div class="container-block">
         <label for="address">
           Endereco
           <textarea id="address" name="address"></textarea>
         </label>
       </div>
 
-      <div class="input-block-container">
+      <div class="container-block">
+        <label for="states">
+          Estado
+          <select name="states" id="states">
+            <option 
+              value="" 
+              selected 
+              disabled 
+              hidden
+            >
+              Selecione um estado
+            </option>
+            <option value="BA">Bahia</option>
+            <option value="ES">Espirito Santo</option>
+            <option value="MG">Minas Gerais</option>
+            <option value="RJ">Rio de Janeiro</option>
+            <option value="SP">São Paulo</option>
+          </select>
+        </label>
+      </div>
 
-        <div class="input-block">
-          <label for="states">
-            Estado
-            <select name="states" id="states">
-              <option 
-                value="" 
-                selected 
-                disabled 
-                hidden
-              >
-                Selecione um estado
-              </option>
-              <option value="BA">Bahia</option>
-              <option value="ES">Espirito Santo</option>
-              <option value="MG">Minas Gerais</option>
-              <option value="RJ">Rio de Janeiro</option>
-              <option value="SP">São Paulo</option>
-            </select>
-          </label>
-        </div>
-
-        <div class="input-block">
-          <label for="date">
-            Data Nasc.
-            <input id="date" name="date" type="date">
-          </label>
-        </div>
+      <div class="container-block">
+        <label for="date">
+          Data Nasc.
+          <input id="date" name="date" type="date">
+        </label>
+      </div>
         
-        <div class="input-block">
-          <label for="">
-            Sexo
+      <div class="container-block">
+        <label for="">
+          Sexo
 
-            <div>
-              <input 
-                name="sex" 
-                value="M" 
-                type="radio"
-              >
-              Masculino
-            </div>
+          <div>
+            <input 
+              name="sex" 
+              value="M" 
+              type="radio"
+            >
+            Masculino
+          </div>
 
-            <div>
-              <input 
-                name="sex" 
-                value="F" 
-                type="radio"
-                checked
-              >
-              Feminino
-              </div>
-          </label>
-        </div>
+          <div>
+            <input 
+              name="sex" 
+              value="F" 
+              type="radio"
+              checked
+            >
+            Feminino
+          </div>
+        </label>
+      </div>
 
-        <div class="input-block">
-          <label for="">
-            Áreas de Interesse
+      <div class="container-block">
+        <label for="">
+          Áreas de Interesse
 
-            <div>
-              <input name="checkMovieTheater" type="checkbox" value="true">
-              Cinema
-            </div>
+          <div>
+            <input name="checkMovieTheater" type="checkbox" value="true">
+            Cinema
+          </div>
 
-            <div>
-              <input name="checkMusic" type="checkbox" value="true">
-              Musica
-            </div>
+          <div>
+            <input name="checkMusic" type="checkbox" value="true">
+            Musica
+          </div>
 
-            <div>
-              <input name="checkInfo" type="checkbox" value="true">
-              Informática
-            </div>
+          <div>
+            <input name="checkInfo" type="checkbox" value="true">
+            Informática
+          </div>
           
-          </label>
-        </div>
+        </label>
       </div>
       
-      <div class="input-block">
+      <div class="container-block">
         <label for="login">
           Login
           <input id="login" type="text" name="login">
         </label>
       </div>
 
-      <div class="input-block">
+      <div class="container-block">
         <label for="password">
           Senha
           <input id="password" type="password" name="password">
         </label>
       </div>
       
-      <div class="input-block">
+      <div class="container-block">
         <label for="password2">
-          Senha
+          Confirmar senha
           <input id="password2" type="password" name="password2">
         </label>
       </div>
 
-      <div class="input-block">
+      <div class="container-block btn">
         <input 
           class="btn" 
           id="btnSubmit" 
