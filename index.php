020e6edad2427ec4c2c3fb3e5cd11d3fd3253f14
Almerindo Paixão <almerindopaixao@gmail.com>
2020-08-11 18:19:07 -0300

    First Commit

---
+++ index.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="author" content="Almerindo da Paixão Junior">
+  <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@400;500;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="assets/css/index.css">
+  <title>Formulario de Cadastro</title>
+</head>
+<body>
+  <main id="root">
+    <form 
+      action="src/controllers/cadCliente.php" 
+      method="POST"
+      name="registrationForm"
+      class="registrationForm"
+    >
+      <h1>Cadastro de clientes</h1>
+
+      <div class="input-block">
+        <label for="name">
+          Nome
+          <input id="name" type="text" name="name">
+        </label>
+      </div>
+
+      <div class="input-block">
+        <label for="cpf">
+          CPF
+          <input 
+            id="cpf" 
+            type="text" 
+            name="cpf" 
+            maxlength="14"
+          >
+        </label>
+      </div>
+
+      <div class="input-block">
+        <label for="address">
+          Endereco
+          <textarea id="address" name="address"></textarea>
+        </label>
+      </div>
+
+      <div class="input-block-container">
+
+        <div class="input-block">
+          <label for="states">
+            Estado
+            <select name="states" id="states">
+              <option 
+                value="" 
+                selected 
+                disabled 
+                hidden
+              >
+                Selecione um estado
+              </option>
+              <option value="BA">Bahia</option>
+              <option value="ES">Espirito Santo</option>
+              <option value="MG">Minas Gerais</option>
+              <option value="RJ">Rio de Janeiro</option>
+              <option value="SP">São Paulo</option>
+            </select>
+          </label>
+        </div>
+
+        <div class="input-block">
+          <label for="date">
+            Data Nasc.
+            <input id="date" name="date" type="date">
+          </label>
+        </div>
+        
+        <div class="input-block">
+          <label for="">
+            Sexo
+
+            <div>
+              <input 
+                name="sex" 
+                value="M" 
+                type="radio"
+              >
+              Masculino
+            </div>
+
+            <div>
+              <input 
+                name="sex" 
+                value="F" 
+                type="radio"
+                checked
+              >
+              Feminino
+              </div>
+          </label>
+        </div>
+
+        <div class="input-block">
+          <label for="">
+            Áreas de Interesse
+
+            <div>
+              <input name="checkMovieTheater" type="checkbox" value="true">
+              Cinema
+            </div>
+
+            <div>
+              <input name="checkMusic" type="checkbox" value="true">
+              Musica
+            </div>
+
+            <div>
+              <input name="checkInfo" type="checkbox" value="true">
+              Informática
+            </div>
+          
+          </label>
+        </div>
+      </div>
+      
+      <div class="input-block">
+        <label for="login">
+          Login
+          <input id="login" type="text" name="login">
+        </label>
+      </div>
+
+      <div class="input-block">
+        <label for="password">
+          Senha
+          <input id="password" type="password" name="password">
+        </label>
+      </div>
+      
+      <div class="input-block">
+        <label for="password2">
+          Senha
+          <input id="password2" type="password" name="password2">
+        </label>
+      </div>
+
+      <div class="input-block">
+        <input 
+          class="btn" 
+          id="btnSubmit" 
+          type="submit" 
+          name="btnSubmit" 
+          value="Enviar"
+        >
+
+        <input 
+          class="btn"
+          id="btnReset" 
+          type="reset" 
+          name="btnReset" 
+          value="Reset"
+        >
+      </div>
+      
+    </form>
+  </main>
+</body>
+</html>
---
+++ src/controllers/cadCliente.php
@@ -0,0 +1,5 @@
+<?php
+
+  echo 'Estamos na pagina cadCliente.php';
+
+?>
